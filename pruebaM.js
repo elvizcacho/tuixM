8193cc168114e4ee56b249df4cb95a478ba23853
Sebastian Vizcaino <elvizcacho@gmail.com>
2014-07-06 11:58:05 -0500

    I change the sctructure of the library to not modife Array prototype.

--- pruebaM.js
+++ pruebaM.js
@@ -1,5 +1,5 @@
 
-var vector = [];
+/*var vector = [];
 var i;
 vector.init(10,0);
 document.writeln();
@@ -23,4 +23,36 @@ matrix3.initM(3,3,0);
 
 matrix3.multM(matrix1,matrix2);
 document.writeln();
-matrix3.writeM();
+matrix3.writeM();*/
+
+var v1 = new Vector([1,2,3,4,5,6,7,8,9]);
+document.writeln(v1[0]);
+document.writeln('longitud de v1 '+v1.length);
+var i;
+for(i=0;i<v1.length;i++){
+  document.write(v1[i]);
+}
+
+var m1 = new Matrix(10.0,5);
+var m2 = new Matrix([[1,2,3],[4,5,6],[7,8,9]]);
+document.writeln('\nfilas de m1 ' + m1.getNumberOfRows());
+document.writeln('\nfilas de m2 ' + m2.getNumberOfRows());
+m1.writeM();
+document.writeln();
+m2.writeM();
+document.writeln('m1[0][0] vale: '+m1[0][0]);
+var m3 = Mmath.transM(m2);
+m3.writeM();
+var m4 = Mmath.toEach(m3,function(num){
+  return num+=2;
+});
+document.writeln('m1[0][0] vale: '+m1[0][0]);
+m4.writeM();
+document.writeln('////////fbM//////');
+var m5 = Mmath.sum(m3,m4);
+m5.writeM();
+var m6 = Mmath.mult(m3,m4);
+document.writeln('////////fbM//////');
+m6.copy(m2);
+m6.writeM();
+
--- tuixM.js
+++ tuixM.js
@@ -1,102 +1,141 @@
-//adds a vector initialization function to the Array prototype.
-Array.prototype.init=function(longitud,initNum){
-  var i;
-  for(i=0;i<longitud;i++){
-    this[i]=initNum;
-   }
-}
+//vector constructor
+var Vector = function(l){
+ var i;
+  document.writeln(typeof l);
+  if(typeof l === 'number'){
+    for(i=0;i<l;i++){
+      this[i]=0;
+    }
+    this.length=l;
+  }
+  else if(typeof l === 'object'){
+    for(i=0;i<l.length;i++){
+      this[i]=l[i];
+    }
+    this.length=l.length;
+  }
+};
+Vector.prototype = new Array;
 
-//adds to Array prototype an matrix initializing function =)
-Array.prototype.initM = function(m,n,initNum){
+/////////////////////////////////////MATRIXES///////////////////////
+//matrix constructor
+var Matrix = function(row,col){
   var i;
   var j;
   var a=[];
-  for(j=0;j<m;j++){
-    a=[];
-    for(i=0;i<n;i++){
-      a[i]=initNum;
+
+    this.getNumberOfRows=function(){
+      if(typeof row === 'number' && typeof col === 'number'){
+        return row;
+      }
+      else{
+        return row.length;
+      }
     }
-    this[j]=a;
+  
+    this.getNumberOfColums=function(){
+      if(typeof row === 'number' && typeof col === 'number'){
+        return col;
+      }
+      else{
+        return row[0].length;
+      }
+    }
+
+  if(typeof row === 'number' && typeof col === 'number'){
+    for(j=0;j<row;j++){
+      a=[];
+      for(i=0;i<col;i++){
+        a[i]=0;
+      }
+      this[j]=a;
+      this[j].length=a.length;
+    }
+    this.length=row;
   }
-}
-//adds to Array prototype a function to write the whole matrix
-Array.prototype.writeM = function(){
- for(j=0;j<this.length;j++){
-  for(i=0;i<this[j].length;i++){
+  else if(typeof row === 'object'){
+   for(j=0;j<row.length;j++){
+      a=[];
+      for(i=0;i<row[0].length;i++){
+        a[i]=row[j][i];
+      }
+      this[j]=a;
+      this[j].length=row[j].length;
+    }
+    this.length=row.length;
+  }
+};
+Matrix.prototype = new Array;
+
+
+//writes the whole matrix in the document.
+Matrix.prototype.writeM = function(){
+ for(j=0;j<this.getNumberOfRows();j++){
+  for(i=0;i<this.getNumberOfColums();i++){
     document.write(this[j][i]+' ');
   }
   document.write('\n\n');
  }
 }
 
-Array.prototype.copy = function(matriz){//copies 'matriz' to this
+Matrix.prototype.copy = function(matriz){//copies 'matriz' to Matrix
   var i;
   var j;
-  for(j=0;j<this.length;j++){
-    for(i=0;i<this[j].length;i++){
+  for(j=0;j<this.getNumberOfRows();j++){
+    for(i=0;i<this.getNumberOfColums();i++){
       this[j][i]=matriz[j][i];
     }
   }
 }
-
-Array.prototype.transM = function(){//gets the transpose of a matrix
+////////////////////////////////MATRIX MATH////////////////////////////////
+var Mmath = {
     
-  if(this.length > 0){
-    if(this[0].length > 1){
-      var auxMatrix = [];
-      auxMatrix.initM(this.length,this[0].length,0);
-      var i,j;
-      for(i=0;i<this.length;i++){
-        for(j=0;j<this[i].length;j++){
-         auxMatrix[i][j] = this[j][i];
+  transM:function(matrix){//returns the matrix transpose of matrix.
+    var result = new Matrix(matrix.getNumberOfRows(),matrix.getNumberOfColums());
+    var i,j;
+    for(i=0;i<matrix.length;i++){
+      for(j=0;j<matrix[i].length;j++){
+       result[i][j] = matrix[j][i];
+      }
+    }
+    return result;
+  },
+  toEach:function(matrix,f){//applies f to each matrix cell
+    var result = new Matrix(matrix.getNumberOfRows(),matrix.getNumberOfColums());
+    var i,j;
+    for(j=0;j<matrix.length;j++){
+      for(i=0;i<matrix[j].length;i++){
+        result[j][i] = f(matrix[j][i]);
+      }
+    }
+    return result;
+  },
+  fbM:function(matrix1,matrix2,f){//does f between the cells of matrix1 and matrix2
+    var result = new Matrix(matrix1.getNumberOfRows(),matrix1.getNumberOfColums());
+    var i,j;
+    for(j=0;j<matrix1.length;j++){
+      for(i=0;i<matrix1[j].length;i++){
+        result[j][i] = f(matrix1[j][i],matrix2[j][i]);
+      }
+    }
+    return result;   
+  },
+  sum:function(matrix1,matrix2){//matrix addition
+    return this.fbM(matrix1,matrix2,function(num1,num2){
+             return num1 + num2;
+           });
+  },
+  mult:function(matrix1,matrix2){//matrix multiplication
+    var result = new Matrix(matrix1.getNumberOfRows(),matrix1.getNumberOfColums());
+    var i,j;
+    var m;
+    for(j=0;j<matrix1.length;j++){
+      for(i=0;i<matrix1[j].length;i++){
+        for(m=0;m<matrix1.length;m++){
+         result[i][j]+=matrix1[i][m]*matrix2[m][j];
         }
       }
-      this.copy(auxMatrix);
     }
-    else{
-      throw 'matrix dimensions are not appropiated.';   
-    }
-  }
-  else{
-    throw 'matrix is not initialized';
-  }
-}
-
-Array.prototype.toEach = function(f){//applies f to each matrix cell
-  var i;
-  var j;
-  for(j=0;j<this.length;j++){
-    for(i=0;i<this[j].length;i++){
-      this[j][i] = f(this[j][i]);
-    }
-  }
-}
-
-Array.prototype.fbM = function(f,matrix){//does f between  matrix and this cells
-  var i;
-  var j;
-  for(j=0;j<this.length;j++){
-    for(i=0;i<this[j].length;i++){
-      this[j][i] = f(this[j][i],matrix[j][i]);
-    }
-  }
-}
-
-Array.prototype.sumM = function(matrix){//matrix addition
-  this.fbM(function(num1,num2){
-  return num1 + num2;
-  },matrix);
-}
-
-Array.prototype.multM = function(matrix1,matrix2){//matrix multiplication
-  var i;
-  var j;
-  var m;
-  for(j=0;j<matrix1.length;j++){
-    for(i=0;i<matrix1[j].length;i++){
-      for(m=0;m<matrix1.length;m++){
-       this[i][j]+=matrix1[i][m]*matrix2[m][j];
-      }
-    }
+    return result;
   }
 }
