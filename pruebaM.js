dc8358efd36578c554b9ec264f87d7e5ec4b21f4
Sebastian Vizcaino <elvizcacho@gmail.com>
2014-07-06 12:03:20 -0500

    I remove the comentary on pruebaM.js wichi was useless

--- pruebaM.js
+++ pruebaM.js
@@ -1,30 +1,4 @@
 
-/*var vector = [];
-var i;
-vector.init(10,0);
-document.writeln();
-document.writeln('vector functions\n');
-for(i=0;i<vector.length;i++){
-  document.write(vector[i]);
-}
-document.writeln();
-document.writeln('\nmatrix functions\n');
-document.writeln('initM(fil,col,initNum); creates a matrix\ne.g:matrix1.initM(10,10,0);\nIf you wanna write the matrix in the document, you can use: writeM(); \ne.g:matrix1.writeM(); you can see the result below.\n');
-var matrix1 = [[5,3,8],
-[6,2,4],[5,2,7]];
-var matrix2 = [[3,2,5],
-[8,2,1],
-[0,9,10]];
-matrix1.writeM();
-document.writeln();
-matrix2.writeM();
-matrix3 = [];
-matrix3.initM(3,3,0);
-
-matrix3.multM(matrix1,matrix2);
-document.writeln();
-matrix3.writeM();*/
-
 var v1 = new Vector([1,2,3,4,5,6,7,8,9]);
 document.writeln(v1[0]);
 document.writeln('longitud de v1 '+v1.length);
